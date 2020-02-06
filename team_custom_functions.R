d03ff3b7ec2dd128d40a78470c732c077ccb18f0
Dan <7553647+dcfaltesek@users.noreply.github.com>
2020-02-06 08:54:06 -0800

    Add files via upload

---
+++ team_custom_functions.R
@@ -0,0 +1,59 @@
+#this function will take any dataframe from rtweet and render our prefered base structure
+address_network<-function(x){
+  f_load<-x$text
+  address_pattern<-"@[[:graph:]]+"
+  f_internal<-stringr::str_extract_all(f_load, address_pattern)
+  f_internal
+  f_list<-reshape2::melt(f_internal)
+  f_passing<-data.frame(f_list)
+  f_prime<-dim(x)[1]
+  f_prime2<-1:f_prime
+  f_rugged<-data.frame(x$text, f_prime2, x$created_at, x$screen_name)
+  f_rugged
+  colnames(f_passing)[2]<-"f_prime2"
+  f_final<-dplyr::inner_join(f_rugged, f_passing, by="f_prime2")
+  f_final
+}  
+
+#this function renders the prepared network as an edgelist, you can then use it in gephi
+format_network<-function(x){
+  f_source<-x$x.screen_name
+  f_target<-stringr::str_replace_all(x$value, "@", "")
+  data.frame(SOURCE=f_source, TARGET=f_target, tweet_number=x$f_prime2)
+}
+
+
+#produces a DFM of a twitter corpus
+topic_corpus<-function(x){
+  f_7<-quanteda::corpus(x$text)
+  f_8<-dfm(f_7)
+}
+
+#be sure to store the result
+some_storage_var<-topic_corpus(yourcorpus)
+
+#load your dfm object as X, number of topics as Y, number of terms z
+#the result 
+topic_blast<-function(x, y, z){
+  internal_X <- dfm(x, remove = stopwords("english"))
+  internal_X2 <- dfm_trim(internal_X, min_termfreq = 4, max_docfreq = 10)
+  set.seed(100)
+  if (require(topicmodels)) {
+    internal_X3 <- LDA(convert(internal_X2, to = "topicmodels"), k = y)
+    topic_terms<-get_terms(internal_X3, z)
+  }
+  internal_X4<-topics(internal_X3)
+  internal_X5<-data.frame(internal_X4)
+  internal_X6<-row.names(internal_X5)
+  internal_X7<-stringr::str_replace_all(internal_X6, "text", "")
+  topic_assignments<-data.frame(doc_number=internal_X7, doc_topic=internal_X4)
+  returns<-list(topic_terms=topic_terms, topic_assignments=topic_assignments)
+}
+
+#example
+stuff<-topic_blast(corpusname, topics, terms)
+
+
+
+
+
