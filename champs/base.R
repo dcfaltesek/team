e64b31ddda7166f76a1c4c607a677bd55b00c53c
Dan <danielfaltesek@gmail.com>
2020-03-14 15:55:23 -0700

    data code
    Loaded data into meaningful variable names, added todo list

--- champs/base.R
+++ champs/base.R
@@ -7,7 +7,28 @@ library(textrecipes)
 library(parsnip)
 library(ggplot2)
 
-#so we need to get a sense of how we will evaluate matchups
-#a few ideas
+#so in the base directory I loaded a really old tournment
+#these should get the datasets from your local github source
+`results` <- read.csv("~/Documents/GitHub/team/champs/2011NCAA.csv", stringsAsFactors=FALSE)
+`teams` <- read.csv("~/Documents/GitHub/team/champs/2011NCAATEAMS.csv", stringsAsFactors=FALSE)
 
-#we could do machine learning methods that would suppose that we look at QUALITIES of teams
+#notice that I stored these in really nice names
+
+View(results)
+View(teams)
+
+#first list of tasks to do..
+#1. clean the results from the games
+#2. clean the team names in the teams so that NCAA is used to indicate that they made the touranment
+#3. structure the results so that the winning team is ALWAYS on the left and the loser is on the right
+#4. more advanced things to think about, how to we model each game, do we run a model on who wins and loses?
+
+
+
+
+
+
+
+
+
+
