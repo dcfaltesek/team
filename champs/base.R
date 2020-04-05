fb10287c56b5b3f0cac8a5c1ae9caf36a0b6263e
paradoxus11 <62157074+paradoxus11@users.noreply.github.com>
2020-04-05 13:30:56 -0700

    Update base.R

--- champs/base.R
+++ champs/base.R
@@ -75,13 +75,59 @@ results%>%
   select(Schl, Opp)
 
 
+#Simon here, I wrote some code to get winners and losers sorted. I don't know if I did it correctly, but I put it below
+
+
+#Determine if the team in the Schl column won or lost
+
+bb_D<-str_detect(rhi$Result., "W")
+D_factor<-as.factor(bb_D)
+
+#Make column a data frame
+
+D.frame <- as.data.frame(D_factor)
+
+View(D.frame)
+
+#create a shared column between the true/false data frame and rhi
+
+D.frame2 <- cbind(D.frame, "GmNumber"=1:nrow(D.frame)) 
+
+View(D.frame2)
+
+rhi3<-rhi
+
+View(rhi3)
+
+#bind the data frames by the reference column 
+
+WonGames <-bind_cols(data.frame(D.frame2), data.frame(rhi3))
+
+View(WonGames)
+
+#Swapping Schl and Opp column data if the team on the right won, so all winners are on the left and all losers are on the right
+
+TransGames <- transform(WonGames, Schl = ifelse(D_factor == 'FALSE', Opp, Schl), Opp = ifelse(D_factor == 'FALSE', Schl, Opp))
+
+View(TransGames)
+
+#select out school, opp, T_factor, and D_factor
+
+Results <- select(TransGames, Schl, Opp, D_factor, T_factor)
+
+View(Results)
+
+#This gives a data frame with winners, losers, and if the winner was the expected team to win
+
+colnames(Results)[1]<-"Winner" 
+colnames(Results)[2]<-"Loser"
+colnames(Results)[3]<-"Winner Expected"
 
 
 
 
 
 
-View(rhi)
 
 
 
