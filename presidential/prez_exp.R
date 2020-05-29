e7d238e24c2107c6bb7a0577d34fb9d1d844bb81
Dan <danielfaltesek@gmail.com>
2020-05-29 16:28:41 -0700

    Update prez_exp.R

--- presidential/prez_exp.R
+++ presidential/prez_exp.R
@@ -7,25 +7,34 @@ library(ggplot2)
 #adds paragraph numbers and speech names
 reference<-1:66
 
+#this dataframe has the paragraph numbers
 farewell_2<-data.frame(farewell, reference)
 
-
+#breaks apart the pargraphs by word and then counts them by pargraph 
 farewell_words<-farewell_2%>%
   unnest_tokens(word, paragraph)%>%
   count(reference, word, sort = TRUE)
 
 #all words are counted by paragraph, ref is paragraph number, n is count
-
 total_words <- farewell_words %>% 
   group_by(reference) %>% 
   summarize(total = sum(n))
 
+#join these back to paragraph words, this is key for some measures
 paragraph_words <- left_join(farewell_words, total_words, by="reference")
 
+#get a frequency by rank
 freq_by_rank <- paragraph_words %>% 
   group_by(reference) %>% 
   mutate(rank = row_number(), 
          `term frequency` = n/total)
+
+#frequency by rank 2 which is by the whole document
+freq_by_rank2 <- paragraph_words %>% 
+  group_by(word)%>%
+  summarize(totes=sum(n))%>%
+  arrange(desc(totes))%>%
+  mutate("rank"=row_number(), "term frequency"= totes/sum(totes))
 
 freq_by_rank %>% 
   ggplot(aes(rank, `term frequency`, color = reference)) + 
@@ -33,6 +42,13 @@ freq_by_rank %>%
   scale_x_log10() +
   scale_y_log10()
 View(freq_by_rank)
+
+freq_by_rank2 %>% 
+  ggplot(aes(rank, `term frequency`, color = reference)) + 
+  geom_line(size = 1.1, alpha = 0.8, show.legend = FALSE) + 
+  scale_x_log10() +
+  scale_y_log10()
+
 
 Zprime<-paragraph_words%>%
   bind_tf_idf(word, reference,n)
