a1be4c0ce8367e9762de5691ac449921ba865151
Dan <danielfaltesek@gmail.com>
2020-05-29 13:31:59 -0700

    first code

--- presidential/prez_exp.R
+++ presidential/prez_exp.R
@@ -1,1 +1,11 @@
 #load some libraries
+library(tidytext)
+library(textfeatures)
+library(dplyr)
+library(ggplot2)
+
+#adds paragraph numbers and speech names
+speech_meta<-function(X,Y){
+  Q<-mutate(X, "paragraph"=1:dim(X)[1])
+  R<-mutate(Q, "speech"=Y)
+}
